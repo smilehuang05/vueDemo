728ee11111639426f4d8ba5f426bee69c94dc71a
vivi <huangyh1988@qq.com>
2019-04-04 14:55:01 +0800

    注册全局守卫

--- src/main.js
+++ src/main.js
@@ -9,10 +9,26 @@ import './assets/iconfont/iconfont.css';
 
 
 Vue.use(ElementUI);
-
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+//注册一个全局守卫，作用是在路由跳转前，对路由进行判断，防止未登录的用户跳转到其他页面去
+router.beforeEach((to, from, next) => {
+        let token = localStorage.getItem('myToken');
+        //如果已经登录不干涉你，让你随便访问
+        if (token) {
+            next();
+        } else {
+            if (to.path !== '/login') {
+                //如果没有登录，但你访问其他需要登录的页面，那我就让你调到登录页面去
+                next({ path: '/login' })
+            } else {
+                //如果没有登录，但你访问的login，那就不干涉你，让你 访问
+                next()
+            }
+        }
+
+    })
+    /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
