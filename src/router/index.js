a51bf9120f5582b3f71eb8128ad3a0f0c2dfa530
vivi <huangyh1988@qq.com>
2019-04-08 16:04:14 +0800

    用户列表页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Login from '@/views/Login.vue'
 import Home from '@/views/Home.vue'
 import Welcome from '@/views/welcome/Welcome.vue'
+import User from '@/views/user/User.vue'
 
 Vue.use(Router)
 
@@ -21,6 +22,11 @@ export default new Router({
                     path: 'welcome',
                     name: 'Welcome',
                     component: Welcome
+                }, {
+                    path: 'user',
+                    name: 'User',
+                    component: User
+
                 }
 
             ]
---
+++ src/views/user/User.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="user">
+    <el-row>
+      <el-col :span="24">
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
+          <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+        </el-breadcrumb>
+      </el-col>
+  </el-row>
+
+  <el-row>
+  <el-col :span="24">
+    <div>
+      <el-input placeholder="请输入内容" class="search-input">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+   
+  <el-button type="success" plain>添加用户</el-button>
+  </div>
+  </el-col>
+  </el-row>
+  <el-table :data="tableData" border style="width: 100%">
+    <el-table-column prop="date" label="日期" width="180">
+    </el-table-column>
+    <el-table-column prop="name" label="姓名" width="180">
+    </el-table-column>
+    <el-table-column prop="address" label="地址">
+    </el-table-column>
+  </el-table>
+
+  <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="1"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="10"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="40"
+      class="page">
+    </el-pagination>
+  </div>
+
+</template>
+<script>
+export default {
+   data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    },
+    methods: {
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
+    }
+
+}
+</script>
+<style lang="scss" scoped>
+.user{
+  .search-input{
+    width:300px;
+    margin-bottom: 5px;
+  }
+  .page{
+    margin-top: 5px;
+    background-Color: #D3DCE6;
+    padding: 10px;
+  }
+}
+</style>
+
