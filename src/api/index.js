84175fb761b9b5e17b7eff896394610eea26282d
vivi <huangyh1988@qq.com>
2019-04-09 11:20:02 +0800

    添加和修改用户

--- src/api/index.js
+++ src/api/index.js
@@ -17,10 +17,31 @@ axios.interceptors.request.use(function(config) {
 
 //登录验证
 export const checkUser = params => {
-        return axios.post('login', params).then(res => res.data)
-    }
+    return axios.post('login', params).then(res => res.data)
+}
 
 //获取用户列表
 export const getUserList = params => {
     return axios.get('users', params).then(res => res.data)
+}
+
+//更改用户状态
+export const changeUserState = params => {
+    return axios.put(`users/${params.uid}/state/${params.type}`).then(res => res.data)
+}
+
+//添加用户
+export const addUser = params => {
+    return axios.post('users', params).then(res => res.data)
+}
+
+//根据ID获取用户信息
+export const getUserById = params => {
+    return axios.get(`users/${params}`).then(res => res.data)
+}
+
+//编辑用户信息
+
+export const editUser = params => {
+    return axios.put(`users/${params.id}`, params).then(res => res.data)
 }
